3183faf4746958dc29e308412f5dd52f0ee8fa1b
ababoum <ababou237@hotmail.com>
2023-03-13 17:48:45 +0100

    day00 finished

--- day00/ex04/main.c
+++ day00/ex04/main.c
@@ -3,13 +3,14 @@
 
 #define BUTTON_PIN PD2
 #define LED_PIN PB0
-#define DEBOUNCE_DELAY_MS 10
+#define DEBOUNCE_DELAY_MS 20
 
 // Make the LED (PB0) turn on when button SW1 (PD2) is pressed
 // 0 for low, 1 for high
 // 0 for input, 1 for output
 int main(void)
 {
+    int button_pressed = 0;
     // Set the bit corresponding to PB0 to 1 [output] in the Data Direction Register
     DDRB |= (1 << LED_PIN);
     // Set the bit corresponding to PD2 to 0 [input] in the Data Direction Register
@@ -24,8 +25,14 @@ int main(void)
     {
         if (!(PIND & (1 << BUTTON_PIN))) // button is being pressed
         {
-            PORTB ^= (1 << LED_PIN);
-            _delay_us(1000 * DEBOUNCE_DELAY_MS);
+            if (!button_pressed)
+            {
+                PORTB ^= (1 << LED_PIN);
+                button_pressed = 1;
+            }
+            _delay_ms(DEBOUNCE_DELAY_MS);
         }
+        else
+            button_pressed = 0;
     }
 }
--- day00/ex05/main.c
+++ day00/ex05/main.c
@@ -7,12 +7,14 @@
 #define LED_PIN1 PB1
 #define LED_PIN2 PB2
 #define LED_PIN3 PB4
-#define DEBOUNCE_DELAY_MS 500
+#define DEBOUNCE_DELAY_MS 20
 
 // 0 for low, 1 for high
 // 0 for input, 1 for output
 int main(void)
 {
+    int button_1_pressed = 0;
+    int button_2_pressed = 0;
     unsigned long counter = 0;
 
     DDRB |= (1 << LED_PIN0);
@@ -29,25 +31,36 @@ int main(void)
     while (1)
     {
         // increment the counter
-        if (!(PIND & (1 << BUTTON_PIN_SW1))) // button is being pressed
+        if (!(PIND & (1 << BUTTON_PIN_SW1))) // button SW1 is being pressed
         {
-            if (counter <= 14)
+            if (!button_1_pressed && counter <= 14)
+            {
                 ++counter;
+                button_1_pressed = 1;
+            }
             _delay_ms(DEBOUNCE_DELAY_MS);
         }
-        else if (!(PIND & (1 << BUTTON_PIN_SW2))) // button is being pressed
+        else
+            button_1_pressed = 0;
+        
+        if (!(PIND & (1 << BUTTON_PIN_SW2))) // button SW2 is being pressed
         {
-            if (counter >= 1)
+            if (!button_2_pressed && counter >= 1)
+            {
                 --counter;
+                button_2_pressed = 1;
+            }
             _delay_ms(DEBOUNCE_DELAY_MS);
         }
+        else
+            button_2_pressed = 0;
 
         // Light the LED according to the counter
         if (counter & 0b0001)
             PORTB |= (1 << LED_PIN0);
         else
             PORTB &= ~(1 << LED_PIN0);
-        
+
         if (counter & 0b0010)
             PORTB |= (1 << LED_PIN1);
         else
@@ -57,7 +70,7 @@ int main(void)
             PORTB |= (1 << LED_PIN2);
         else
             PORTB &= ~(1 << LED_PIN2);
-        
+
         if (counter & 0b1000)
             PORTB |= (1 << LED_PIN3);
         else

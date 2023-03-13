345cfb8cc883967e4750657f7c95df6a1d84ee82
ababoum <ababou237@hotmail.com>
2023-03-13 16:04:02 +0100

    day00 in progress - ex03 done

--- day00/ex02/main.c
+++ day00/ex02/main.c
@@ -1,15 +1,19 @@
 #include <avr/io.h>
 
-// Turn on the LED connected to pin PB0
+// Make the LED blink on PB0 with 1 Hz frequency
 // 0 for low, 1 for high
 int main(void)
 {
+    unsigned long i;
+
     // Set the bit corresponding to PB0 to 1 in the Data Direction Register
     DDRB |= (1 << PB0);
     while (1)
     {
+        i = 0;
         // Continuously set the bit corresponding to PB0 to 1 in the Port Register
-        PORTB |= (1 << PB0);
+        PORTB ^= (1 << PB0);
+        while (i < F_CPU / 2)
+            i++;
     }
-    return (0);
 }
---
+++ day00/ex03/main.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+
+// Make the LED (PB0) turn on when button SW1 (PD2) is pressed
+// 0 for low, 1 for high
+int main(void)
+{
+
+    // Set the bit corresponding to PB0 to 1 in the Data Direction Register
+    DDRB |= (1 << PB0);
+    // Set the bit corresponding to PD2 to 1 in the Data Direction Register
+    DDRB |= (1 << PD2);
+    while (1)
+    {
+        // Set the bit corresponding to PB0 to 1 in the Port Register when SW1 is pressed
+        while (!(PIND & (1 << PD2)))
+        {
+            PORTB |= (1 << PB0);
+        }
+        PORTB &= ~(1 << PB0);
+    }
+}

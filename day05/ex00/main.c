6e92260d5eb0db586e7022bea67919db2621f3a1
ababoum <ababou237@hotmail.com>
2023-03-20 15:21:11 +0100

    day05 in progress: day00 finished

--- day05/ex00/main.c
+++ day05/ex00/main.c
@@ -14,7 +14,16 @@
 
 #define EEPROM_COUNTER_ADDRESS 255
 
-void display_counter_LED(unsigned long counter)
+typedef enum {
+    DEC,
+    INC
+} t_counter_direction;
+
+unsigned long counter = 0;
+int button_1_pressed = 0;
+int button_2_pressed = 0;
+
+void display_counter_LED(void)
 {
     if (counter & 0b0001)
         PORTB |= (1 << LED_PIN0);
@@ -37,69 +46,79 @@ void display_counter_LED(unsigned long counter)
         PORTB &= ~(1 << LED_PIN3);
 }
 
-void update_EEPROM_counter(unsigned long *counter, int delta)
+void update_EEPROM_counter(t_counter_direction dir)
 {
-    *counter += delta;
-    eeprom_update_block(counter, (unsigned long *)EEPROM_COUNTER_ADDRESS, sizeof(unsigned long));
+    uart_printstr("Updating EEPROM counter\n\r");
+    uart_printstr("Counter value: ");
+    uart_printnb(counter);
+    uart_printstr("\n\r");
+    if (dir == DEC && counter == 0UL)
+        return;
+    else if (dir == INC && counter + 1 == 16UL)
+        return;
+    
+    dir == INC ? counter++ : counter--;
+    eeprom_update_block(&counter, (unsigned long *)EEPROM_COUNTER_ADDRESS, sizeof(unsigned long));
 }
 
-void monitor_buttons(int *button_1_pressed, int *button_2_pressed, unsigned long *counter)
+void monitor_buttons(void)
 {
     if (!(PIND & (1 << BUTTON_PIN_SW1))) // button SW1 is being pressed
     {
-        if (!*button_1_pressed && *counter <= 14)
+        if (!button_1_pressed)
         {
             uart_printstr("Button 1 pressed\n\r");
-            update_EEPROM_counter(1);
-            *button_1_pressed = 1;
+            update_EEPROM_counter(INC);
+            button_1_pressed = 1;
         }
         _delay_ms(DEBOUNCE_DELAY_MS);
     }
     else
-        *button_1_pressed = 0;
+        button_1_pressed = 0;
     
     if (!(PIND & (1 << BUTTON_PIN_SW2))) // button SW2 is being pressed
     {
-        if (!*button_2_pressed && *counter >= 1)
+        if (!button_2_pressed)
         {
             uart_printstr("Button 2 pressed\n\r");
-            update_EEPROM_counter(-1);
-            *button_2_pressed = 1;
+            update_EEPROM_counter(DEC);
+            button_2_pressed = 1;
         }
         _delay_ms(DEBOUNCE_DELAY_MS);
     }
     else
-        *button_2_pressed = 0;
+        button_2_pressed = 0;
+}
+
+void calibrate_counter(void)
+{
+    if (counter > 15)
+        counter = 15;
+    eeprom_update_block(&counter, (unsigned long *)EEPROM_COUNTER_ADDRESS, sizeof(unsigned long));
 }
 
 int main()
 {
-    unsigned long counter = 0;
-    int button_1_pressed = 0;
-    int button_2_pressed = 0;
-
     uart_init();
 
     DDRB |= (1 << LED_PIN0) | (1 << LED_PIN1) |
             (1 << LED_PIN2) | (1 << LED_PIN3);
 
-    DDRD &= ~(1 << BUTTON_PIN_SW1);
+    DDRD = ~(1 << BUTTON_PIN_SW1) & ~(1 << BUTTON_PIN_SW2);
     PORTD |= (1 << BUTTON_PIN_SW1);
-
-    DDRD &= ~(1 << BUTTON_PIN_SW2);
     PORTD |= (1 << BUTTON_PIN_SW2);
 
     // Init EEPROM address register
-    EEARH = 0;
-    EEARL = EEPROM_COUNTER_ADDRESS;
+    // EEARH = 0;
+    // EEARL = EEPROM_COUNTER_ADDRESS;
 
     // Load counter value from EEPROM
-    counter = 0;
     eeprom_read_block(&counter, (unsigned long *)EEPROM_COUNTER_ADDRESS, sizeof(counter));
+    calibrate_counter();
 
     while (0xDECAF)
     {
-        monitor_buttons(&button_1_pressed, &button_2_pressed, &counter);
-        display_counter_LED(counter);
+        monitor_buttons();
+        display_counter_LED();
     }
 }
